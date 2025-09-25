442cd52766642a1831f79e4dc4d8ade6f8f822ce
Abhi5301 <126357340+Abhi5301@users.noreply.github.com>
2025-09-25 16:58:59 -0400

    fixed repo

--- lab2/lib/Chassis/src/chassis-params.h
+++
@@ -1,7 +0,0 @@
-/**
- * TODO: Adjust these to get good FK updates. They're not horrible, but neither are they good.
- */
-
-const float ROBOT_RADIUS = 6.661;
-const float LEFT_TICKS_PER_CM = 69.4533;
-const float RIGHT_TICKS_PER_CM = 69.4533;
